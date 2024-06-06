18da323acce223a658b3a696194d8499b699bd89
Mike Back <hello@mikeback.me>
2024-06-06 04:32:55 +0000

    refactor: move the winning line to end

--- number_guess.sh
+++ number_guess.sh
@@ -25,7 +25,6 @@ else
   echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# Start game
 # Initialise game variables
 # Define the minimum and maximum values of the range
 MIN_NUMBER=1; MAX_NUMBER=1000
@@ -35,27 +34,37 @@ SECRET_NUMBER=$(( RANDOM % NUMBER_RANGE + MIN_NUMBER ))
 # - zero the number of guesses
 NUMBER_OF_GUESSES=0
 
+# Define the game logic
 PLAY_GAME(){
   GUESS=$1
+  # Increment the NUMBER_OF_GUESSES variable
+  (( NUMBER_OF_GUESSES += 1 ))
+
+  # check if guess is an integer (a valid guess)
   PATTERN='^[1-9][0-9]*$'
 
-  # check if guess is an integer
-  if [[ "$GUESS" =~ $PATTERN   ]]
-    then
-    # It's a valid guess
-    # Increment the NUMBER_OF_GUESSES variable
-    (( NUMBER_OF_GUESSES += 1 ))
+  if [[ ! "$GUESS" =~ $PATTERN   ]]; then
+    # Not a valid guess
+    echo -e "That is not an integer, guess again:"
+    read NEW_GUESS
+    PLAY_GAME $NEW_GUESS
 
-    # Check the guess
-    if [[ $GUESS -eq $SECRET_NUMBER ]]
-      then
-        echo -e "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-        # Save user stats to db:
-        # - Increment number of games and update in db
-        (( GAMES_PLAYED +=1 ))
-        UPDATE_GAMESCOUNT_RESULT=$($PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE username='$USERNAME' ")
+  else
+    # Valid guess
+    # Check the guess against the secret number
 
-        # - Overwrite best game score (if latest is lower than current or if current is zero) and update in db
+    if [[ $GUESS -lt $SECRET_NUMBER  ]]; then
+        echo -e "It's higher than that, guess again:"
+        read NEW_GUESS
+        PLAY_GAME $NEW_GUESS
+
+    elif [[ $GUESS -gt $SECRET_NUMBER  ]]; then
+        echo -e "It's lower than that, guess again:"
+        read NEW_GUESS
+        PLAY_GAME $NEW_GUESS
+
+    else
+        # Overwrite best game score (if latest is lower than current or if current is zero) and update in db
         if [[ $BEST_GAME -eq 0 ]]; then
           BEST_GAME=$NUMBER_OF_GUESSES
           UPDATE_BESTGAME_RESULT=$($PSQL "UPDATE users SET best_game=$BEST_GAME WHERE username='$USERNAME' ")
@@ -64,29 +73,14 @@ PLAY_GAME(){
           UPDATE_BESTGAME_RESULT=$($PSQL "UPDATE users SET best_game=$BEST_GAME WHERE username='$USERNAME' ")
         fi
 
-    elif [[ $GUESS -lt $SECRET_NUMBER  ]]
-      then
-        echo -e "It's higher than that, guess again:"
-        read NEW_GUESS
-        PLAY_GAME $NEW_GUESS
-
-    elif [[ $GUESS -gt $SECRET_NUMBER  ]]
-      then
-        echo -e "It's lower than that, guess again:"
-        read NEW_GUESS
-        PLAY_GAME $NEW_GUESS
-    else
-      echo "Not sure how you got here....?"
+        echo -e "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
     fi
-
-  else
-    # Not a valid guess
-    echo -e "That is not an integer, guess again:"
-    read NEW_GUESS
-    PLAY_GAME $NEW_GUESS
   fi
 }
 
+# Start the game
 echo -e "Guess the secret number between 1 and 1000:"
 read GUESS
+(( GAMES_PLAYED +=1 ))
+UPDATE_GAMESCOUNT_RESULT=$($PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE username='$USERNAME' ")
 PLAY_GAME $GUESS
